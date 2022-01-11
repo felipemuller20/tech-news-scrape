e15698c6ff1c241a8d445452739a72fb891c1f3e
Felipe Muller <felipeavmuller@gmail.com>
2022-01-11 19:51:48 -0300

    Requisito 1 - Funçao fetch

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,19 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
     """Seu código deve vir aqui"""
+    time.sleep(1)
+    try:
+        response = requests.get(url, timeout=3)
+    except requests.Timeout:
+        return None
+    if response.status_code != 200:
+        return None
+
+    return response.text
 
 
 # Requisito 2
