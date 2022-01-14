4afa2a349739396fd3f7ad379c1e60da5feeecff
Felipe Muller <felipeavmuller@gmail.com>
2022-01-14 16:36:29 -0300

    Remove print

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,17 +1,13 @@
 from tech_news.database import search_news
-# https://stackoverflow.com/questions/319426/how-do-i-do-a-case-insensitive-string-comparison
+
 
 # Requisito 6
 def search_by_title(title):
-    insensitive = title.casefold()
-    print (title, insensitive)
     news = search_news({"title": {"$regex": title, "$options": "i"}})
     result = []
     for new in news:
-        print('NEWWWWWWWWWWWWWW', (new["title"], new["url"]))
         tupla = (new["title"], new["url"])
         result.append(tupla)
-    print(result)
     return result    
 
 
