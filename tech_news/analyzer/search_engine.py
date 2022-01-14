3d9b3913fb2b9a066304c8c7e5f0cbbae448bb32
Felipe Muller <felipeavmuller@gmail.com>
2022-01-14 17:03:58 -0300

    Req 7 - busca new por data

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,5 @@
 from tech_news.database import search_news
-
+import time
 
 # Requisito 6
 def search_by_title(title):
@@ -13,7 +13,18 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    # https://stackoverflow.com/questions/16527878/check-if-line-is-a-timestamp-in-python
+    try:
+        time.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+    
+    news = search_news({"timestamp": {"$regex": date}})
+    result = []
+    for new in news:
+        tupla = (new["title"], new["url"])
+        result.append(tupla)
+    return result
 
 
 # Requisito 8
